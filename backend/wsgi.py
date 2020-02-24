b0249613b60f19cc4ae5dc4e86e95db362c5dad8
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-02-23 20:25:19 -0800

    Updated wsgi file

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "react-django-test.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
